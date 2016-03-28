9cd06046e786d40b58bd3ac19108acf9fbfe16d1
rtfuzz <Fuzz@Afonsos-MacBook-Pro.local>
2016-03-27 23:27:27 -0300

    v1.0.2
    v1.0.2

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 
 var krms_config ={			
 	'ApiUrl':"http://easygo.com.br/app/merchantapp/api",	
-	'DialogDefaultTitle':"EasyGO",
+	'DialogDefaultTitle':"EasyGO Comerciante”,
 	'pushNotificationSenderid':"664383538550"
 };
